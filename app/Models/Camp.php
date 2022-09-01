d20f42a3bfe612d7c9848933063aaf330323c927
Sensen <senoagung27@gmail.com>
2022-09-01 15:10:21 +0700

    migration database

---
+++ app/Models/Camp.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Checkout;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Camp extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['title', 'price'];
+
+    public function getIsRegisteredAttribute()
+    {
+        if (!Auth::check()) {
+            return false;
+        }
+
+        return Checkout::whereCampId($this->id)->whereUserId(Auth::id())->exists();
+    }
+}
